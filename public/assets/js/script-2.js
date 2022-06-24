40042e25e8f40fa2896c03bc425f9cb80dff3ed0
DanielBrai <braidaniel2001@gmail.com>
2022-06-24 15:57:30 +0100

    Added 404 page

--- public/assets/js/script-2.js
+++
@@ -1,7 +0,0 @@
-import linkServices  from './script.js'
-
-const services = document.querySelector('#Services')
-services.addEventListener('click', () => {
-    window.location.href = '/'
-    linkServices.scrollIntoView()
-})
--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -113,5 +113,3 @@ const footerEl = document.querySelector('#copyright')
 let year =  new Date().getFullYear();
 
 footerEl.innerHTML = `<p class="text-center font-medium">&copy; Copyright ${year} Gedan Global Options Ltd RC:746602.</p>`
-
-export default linkServices
