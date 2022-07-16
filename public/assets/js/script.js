a00a2a5f94c9cc532a3045287444e1f4029e72c7
Daniel Brai <88239970+Daniel-Brai@users.noreply.github.com>
2022-07-16 10:17:53 +0100

    Update script.js

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -3,7 +3,7 @@ const menu = document.querySelector('#mobile-menu')
 const home = document.querySelector('#Home')
 const about  = document.querySelector('#About')
 const services = document.querySelector('#Services')
-// const work = document.querySelector('#Work')
+const form = document.querySelector('#form')
 const contact = document.querySelector('#Contact')
 
 
@@ -100,6 +100,10 @@ linkContactMobile.addEventListener('click', () => {
   contact.scrollIntoView()
 })
 
+// Prevent reload of form
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+})
 
 // footer date
 const footerEl = document.querySelector('#copyright')
